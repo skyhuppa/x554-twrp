4be5931fa473f52e8deb2583bded43fb9833e443
Adasi_Kaakyire <skyhuppa@hotmail.co.uk>
2022-07-19 16:03:07 -0700

    Update omni_x554.mk

--- omni_x554.mk
+++ omni_x554.mk
@@ -17,6 +17,6 @@
 # Inherit from our custom product configuration
 $(call inherit-product, vendor/omni/config/common.mk)
 
-$(call inherit-product, device/tecno/x554/full_x554.mk)
+$(call inherit-product, device/infinix/x554/full_x554.mk)
 
 PRODUCT_NAME := omni_x554
